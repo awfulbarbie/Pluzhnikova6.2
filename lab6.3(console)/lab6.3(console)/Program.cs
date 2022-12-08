ccf9a8ecde91aad509c510da6bc98fab14aff356
awfulbarbie <119601129+awfulbarbie@users.noreply.github.com>
2022-12-08 05:55:13 +0300

    Add files via upload

---
+++ lab6.3(console)/lab6.3(console)/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6._3_console_
+{
+    internal class Program
+    {
+        static int[,] Input(out int n, out int m)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите размерность двумерного массива (n - количество строк, m - количество столбцов)");
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+                    m = int.Parse(Console.ReadLine());
+                    if (n <= 0 || m <= 0)
+                    {
+                        Console.WriteLine("Ошибка! Размерность массива не может иметь отрицательное или нулевое значение");
+                        continue;
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка! Неверный формат ввода данных");
+                }
+            }
+            int[,] array = new int[n, m];
+            Console.WriteLine("Заполните массив:");
+            try
+            {
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write("a[{0},{1}] = ", i, j);
+                        array[i, j] = int.Parse(Console.ReadLine());
+                    }
+            }
+            catch
+            {
+                Console.WriteLine("Неверные данные!");
+                Environment.Exit(0); //остановка программы
+            }
+
+            return array;
+        }
+
+        static void Print(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++, Console.WriteLine())
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write("{0,5} ", array[i, j]);   //5 - ширина ячейки для одного элемента
+        }
+        static void result(int[,] array)
+        {
+            bool[] col_array = new bool[array.GetLength(1)];    //логический массив с размерностью, равной количеству столбцов массива array
+     
+            for (int i = 0; i < col_array.Length; i++)          //всем элементам массива присваивается значение true
+            {
+                col_array[i] = true;
+            }
+
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    if (array[j, i] >= 0)                      //проверка всех элементов столбцов массива array,
+                                                               //если условие выполняется, то элементу лог. массива присваивается значение false
+                    {
+                        col_array[i] = false;
+                        break;
+                    }
+                }
+            }
+
+            int flag = 0;
+            for (int i = 0; i < array.GetLength(1); i++)       //перебор всех элементов столбцов массива array,
+                                                               //если элементы col_array = true, то вывод
+            {
+                if (col_array[i])
+                {
+                    string output = string.Empty;               //пустая строка
+                    for (int j = 0; j < array.GetLength(0); j++)
+                    {
+                        output += array[j, i] + "\n";
+                    }
+
+                    Console.WriteLine((i + 1) + " столбец состоит из отрицательных чисел:\n" + output + "\n");
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Нет столбцов, в которых все элементы являются отрицательными");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            int m;
+            int[,] Array = Input(out n, out m);
+            Console.WriteLine("Исходный массив:");
+            Print(Array);
+            result(Array);
+        }
+    }
+}
+
---
+++ lab6.4(console)/lab6.4(console)/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6._4_console_
+{
+    internal class Program
+    {
+        static int[][] Input()
+        {
+            int n = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите размерность массива [n]");
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+                    if (n <= 0)
+                    {
+                        Console.WriteLine("Ошибка! Размерность массива не может иметь отрицательное или нулевое значение");
+                        continue;
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка! Неверный формат ввода данных");
+
+                }
+            }
+
+            Console.WriteLine("Введите элемнты массива: ");
+            int[][] array = new int[n][];                         //Ступенчатый массив - массив массивов, в котором длина каждого массива может быть разной.
+                                                                  //Ступенчатый массив может быть использован для составления таблицы из строк разной длины.
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = new int[n];
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write($"a[{i},{j}] = ");
+                        array[i][j] = int.Parse(Console.ReadLine());
+                    }
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Некорректные данные!");
+                Environment.Exit(0); //остановка программы
+            }
+            return array;
+
+        }
+
+        static void Print1(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++, Console.WriteLine())
+                for (int j = 0; j < array[i].Length; j++)
+                    Console.Write("{0,5} ", array[i][j]);   //5 - ширина ячейки для одного элемента
+        }
+
+        static void Print2(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.Write("{0,5} ", array[i]);
+        }
+
+        static int Min(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+                else if (array[i] == min)
+                {
+                    Environment.Exit(0);        //остановка программы
+                }
+            }
+            return min;
+        }
+
+        static int[] Multiply(int[][] array)
+        {
+            int[] mult = new int[array.GetLength(0)];       //Размерность массива mult равна размерности строки массива array
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                mult[i] = 1;
+                for(int j = 0; j < array[i].Length; j++)
+                {
+                    mult[i] *= array[j][i];
+                }
+            }
+            return mult;
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] Array = Input();
+            Console.WriteLine("Исходный массив:");
+            Print1(Array);
+            int[] mult = new int[Array.Length];
+            mult = Multiply(Array);
+            Console.WriteLine("Новый массив:");
+            Print2(mult);
+            Console.WriteLine();
+            int min = Min(mult);
+            Console.WriteLine($"Минимальный элемент нового массива: {min}");
+
+        }
+    }
+}
